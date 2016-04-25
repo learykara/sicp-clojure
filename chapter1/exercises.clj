982ba9111baf5c6d717b25517e1671cd11a5865e
Kara Leary <learykara@gmail.com>
2016-04-25 13:50:45 -1000

    most of section 1.2 exercises

--- chapter1/exercises.clj
+++ chapter1/exercises.clj
@@ -134,3 +134,321 @@
       guess
       (sqrt-iter (improve guess))))
   (sqrt-iter 1.0))
+
+
+;; Section 1.2
+
+
+;; Factorials
+
+;; Method 1: linear recursive process - length of chain of deferred
+;; multiplications (and amount of information needed to keep track of it)
+;; grows linearly with n
+(defn factorial
+  [n]
+  (if (= n 1)
+    1
+    (* n (factorial (- n 1)))))
+
+;; Method 2: linear iterative - state can be summarized by a fixed number
+;; of state variables with a fixed rule that describes how the variables should
+;; be updated from one state to another. Number of steps grows linearly
+;; with n
+(defn fact-iter
+  [product counter max-count]
+  (if (> counter max-count)
+    product
+    (fact-iter (* counter product)
+               (+ counter 1)
+               (max-count))))
+
+(defn factorial
+  [n]
+  (fact-iter 1 1 n))
+
+
+;; Example: Counting Change
+
+(defn count-change
+  [amount]
+  (cc amount 5))
+
+(defn cc
+  [amount kinds-of-coins]
+  (cond (= amount 0)
+        1
+        (or (< amount 0) (= kinds-of-coins 0))
+        0
+        :else
+        (+ (cc amount (- kinds-of-coins 1))
+           (cc (- amount (first-denomination kinds-of-coins)) kinds-of-coins))))
+
+(defn first-denomination
+  [kinds-of-coins]
+  (cond (= kinds-of-coins 1)
+        1
+        (= kinds-of-coins 2)
+        5
+        (= kinds-of-coins 3)
+        10
+        (= kinds-of-coins 4)
+        25
+        (= kinds-of-coins 5)
+        50))
+
+
+;; 1.11
+
+;; Recursive process
+(defn my-fn
+  [n]
+  (cond (< n 3)
+        n
+        :else
+        (+ (my-fn (- n 1))
+           (* 2 (my-fn (- n 2)))
+           (* 3 (my-fn (- n 3)))
+        )
+  )
+)
+
+;; Iterative process
+(defn my-fn-iter
+  [a b c count]
+  (if (= count 0)
+    a
+    (my-fn-iter b c (+ c (* 2 b) (* 3 a)) (- count 1))))
+
+(defn my-fn-new
+  [n]
+  (my-fn-iter 0 1 2 n))
+
+
+;; 1.12
+
+(defn pascal-tri
+  [row col]
+  (cond (> col row)
+        0
+        (< col 0)
+        0
+        (= col 1)
+        1
+        :else
+        (+ (pascal-tri (- row 1) (- col 1))
+           (pascal-tri (- row 1) col))))
+
+;; 1.15
+(defn cube
+  [x]
+  (* x x x))
+
+(defn p
+  [x]
+  (- (* 3 x) (* 4 (cube x))))
+
+(defn sine
+  [angle]
+  (if (not (> (Math/abs angle) 0.1))
+    angle
+    (p (sine (/ angle 3.0)))))
+
+;; 1.16
+(defn is-even
+  [x]
+  (= (mod x 2) 0))
+
+(defn exp-iter
+  [a b n]
+  (cond (= n 0)
+        a
+        (is-even n)
+        (exp-iter a (square b) (/ n 2))
+        :else
+        (exp-iter (* a b) b (- n 1))))
+
+(defn fast-exp
+  "Compute b^n"
+  [b n]
+  (exp-iter 1 b n))
+
+
+;; 1.17
+(defn times-2
+  [x]
+  (+ x x))
+
+(defn halve
+  [x]
+  (/ x 2))
+
+(defn mult
+  [a b]
+  (mult-iter 0 a b))
+
+(defn mult-iter
+  [a b]
+  (cond (= b 0)
+        0
+        (is-even b)
+        (times-2 (mult-iter a (halve b)))
+        :else
+        (+ a (mult-iter a (- b 1)))))
+
+
+;; 1.18
+(defn mult-iter
+  [s a b]
+  (cond (= b 0)
+        s
+        (even b)
+        (mult-iter s (times-2 a) (halve b))
+        :else
+        (mult-iter (+ s a) a (- b 1))))
+
+
+;; 1.19
+;; lol no
+
+;; 1.20
+;; Normal order: "fully expand and then reduce"
+;; Applicative order: "evaluate arguments then apply"
+;; if statements: predicate expression is evaluated first, and the result
+;; determines whether to evaluate the consequent or alternative expression
+(defn gcd
+  [a b]
+  (if (= b 0)
+    a
+    (gcd b (mod a b))))
+
+;; normal order
+(gcd 206 40)
+
+(gcd 40 (mod 206 40))
+
+(if (= (mod 206 40) 0) 0) ; one
+(gcd (mod 206 40) (mod 40 (mod 206 40)))
+
+(if (= (mod 40 (mod 206 40))) 0) ; two
+(gcd (mod 40 (mod 206 40)) (mod (mod 206 40) (mod 40 (mod 206 40))))
+
+(if (= (mod (mod 206 40) (mod 40 (mod 206 40))) 0)) ; four
+(gcd (mod (mod 206 40) (mod 40 (mod 206 40))) (mod (mod 40 (mod 206 40)) (mod (mod 206 40) (mod 40 (mod 206 40)))))
+
+(if (= (mod (mod 40 (mod 206 40)) (mod (mod 206 40) (mod 40 (mod 206 40)))) 0)) ; seven
+(mod (mod 206 40) (mod 40 (mod 206 40))) ; four
+;; `mod` performed 18 times in normal order
+
+;; applicative order
+(gcd 206 40)
+(gcd 40 (mod 206 40)) ; one
+(gcd 40 6)
+(gcd 6 (mod 40 6)) ; one
+(gcd 6 4)
+(gcd 4 (mod 6 4)) ; one
+(gcd 4 2)
+(gcd 2 (mod 4 2)) ; one
+(gcd 2 0)
+2
+;; `mod` performed 4 times in applicative order
+
+
+;; Searching for divisors
+(defn divides
+  [a b]
+  (= (mod b a) 0))
+
+(defn find-divisor
+  [n test-divisor]
+  (cond (> (square test-divisor) n)
+        n
+        (divides test-divisor n)
+        test-divisor
+        :else
+        (find-divisor n (+ test-divisor 1))))
+
+(defn smallest-divisor
+  [n]
+  (find-divisor n 2))
+
+(defn is-prime
+  [n]
+  (= n (smallest-divisor n)))
+
+;; 1.21
+(smallest-divisor 199) ;; 199
+(smallest-divisor 1999) ;; 1999
+(smallest-divisor 19999) ;; 7
+
+;; Fermat's Little Theorem
+(defn expmod
+  [base exp m]
+  (cond (= exp 0)
+        1
+        (is-even exp)
+        (mod (square (expmod base (/ exp 2) m)))
+        :else
+        (mod (* base (expmod base (- exp 1) m)) m)))
+
+(defn fermat-test
+  [n]
+  (defn try-it
+    [a]
+    (= (expmod a n n) a))
+  (try-it (+ 1 (rand-int (- n 1)))))
+
+(defn fast-prime
+  [n times]
+  (cond (= times 0)
+        true
+        (fermat-test n)
+        (fast-prime n (- times 1))
+        :else
+        false))
+
+
+;; 1.22
+(defn report-prime
+  [elapsed-time]
+  (pprint " *** ")
+  (pprint elapsed-time))
+
+(defn start-prime-test
+  [n start-time]
+  (if (is-prime n)
+    (report-prime (- (time) start-time))))
+
+(defn timed-prime-test
+  [n]
+  (newline)
+  (pprint n)
+  (start-prime-test n (time)))
+
+
+;; 1.23
+(defn next
+  [n]
+  (if (= 2 n)
+    3
+    (+ n 2)))
+
+(defn divides
+  [a b]
+  (= (mod b a) 0))
+
+(defn find-divisor-next
+  [n test-divisor]
+  (cond (> (square test-divisor) n)
+        n
+        (divides test-divisor n)
+        test-divisor
+        :else
+        (find-divisor n (next n))))
+
+(defn smallest-divisor-next
+  [n]
+  (find-divisor-next n 2))
+
+(defn timed-prime-test-next
+  [n]
+  (= n (smallest-divisor-next n)))
